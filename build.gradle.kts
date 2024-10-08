403eb410660f3be012817cc5c6555bd919710cdc
Andre Weber <138565883+wba2hi@users.noreply.github.com>
2024-10-08 10:31:38 +0200

    Generate Lockfile for FOSSLENS (#5)
    * Generate Lockfile for FOSSLENS
    
    The following PR enables Gradles Lockfile mechanism to create
    a file named gradle.lockfile for each subproject. The gradle.lockfile
    is used by FOSSLENS to detect the used 3rd-party dependencies.
    
    * Update Lockfile

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,3 +1,19 @@
+/*
+ * Copyright (c) 2024 Contributors to the Eclipse Foundation
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Apache License, Version 2.0 which is available at
+ * https://www.apache.org/licenses/LICENSE-2.0.
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations
+ * under the License.
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
 import io.gitlab.arturbosch.detekt.Detekt
 import io.gitlab.arturbosch.detekt.DetektCreateBaselineTask
 
@@ -72,5 +88,11 @@ subprojects {
         tasks.named("check") {
             finalizedBy(rootProject.tasks.named("detekt"))
         }
+    }
+
+    // https://docs.gradle.org/current/userguide/dependency_locking.html
+    dependencyLocking {
+        lockAllConfigurations()
+        lockFile = file("$projectDir/gradle.lockfile")
     }
 }
